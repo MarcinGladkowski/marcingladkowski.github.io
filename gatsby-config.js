975216982ccb974b0f8cd86b5cfe0ad76b866d3a
MarcinGladkowski <marcingladkowski@gmail.com>
2021-11-11 22:30:50 +0100

    replace plugin for google analitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,10 +51,21 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `G-ZEP2JBBGT2`,
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
+        // your google analytics tracking id
         trackingId: `G-ZEP2JBBGT2`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
       },
     },
     {
