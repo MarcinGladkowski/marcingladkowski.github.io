83b4cf31abcd6aab23ee6e199bc1bba5e7f3453a
MarcinGladkowski <marcingladkowski@gmail.com>
2021-11-14 10:02:53 +0100

    formatting: format style

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,9 +7,9 @@ const Layout = ({ location, title, children }) => {
   let header
 
   header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
+    <h1 className="main-heading">
+      <Link to="/">{title}</Link>
+    </h1>
   )
 
   return (
